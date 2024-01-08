747c910837a439edbff5e95a0927add817d64d88
Luciano Walter Iasi <iasi@fktech.net>
2024-01-08 10:14:59 -0300

    Inicio el proyecto en git

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typedText', {
+          strings: ['Soy Anto', 'Soy diseñadora UX/UI', 'Soy maquetadora front end'],
+          typeSpeed: 50,
+          loop: true,
+          smartBackspace: true,
+});
